c1ddb1ffccdaac58165b36c3c3c56a8a1170100e
JaggerJoe <gregoire.thomassin@e-artsup.net>
2021-03-17 16:11:36 +0100

    Récuperation des assetes de l'ancien space shooter

---
+++ SpaceShooter/NewSapceShooter/Script/Bullet/BulletDetection.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletDetection : MonoBehaviour
+{
+    [SerializeField]
+    private int m_Damages = 1;
+
+    [SerializeField]
+    private float m_Radius = .5f;
+
+    [SerializeField]
+    private LayerMask m_Layer = 0;
+
+    private Enemy m_Enemy = null;
+
+    private HealthPlayer m_PlayerLife = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SetDamages(m_Damages);
+    }
+
+    private void SetDamages(int p_Damages)
+    {
+        Collider[] l_HitCollider = Physics.OverlapSphere(transform.position, m_Radius, m_Layer);
+        foreach (var item in l_HitCollider)
+        {
+            if(item.GetComponent<Enemy>() != null)
+            {
+                m_Enemy = item.GetComponent<Enemy>();
+                m_Enemy.TakeDamages(p_Damages);
+                Destroy(gameObject);
+            }
+
+            if(item.GetComponent<HealthPlayer>() != null)
+            {
+                m_PlayerLife = item.GetComponent<HealthPlayer>();
+                m_PlayerLife.TakesDamages(p_Damages);
+                Destroy(gameObject);
+            }
+
+            Debug.Log("j'ai touché : " + item.gameObject.name);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, m_Radius);
+    }
+}
---
+++ SpaceShooter/NewSapceShooter/Script/Enemy/EnemyShoot.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShoot : MonoBehaviour
+{
+    [SerializeField]
+    private float m_TimeBeforeNextFire = 3;
+
+    [SerializeField]
+    private GameObject m_BulletEnemy = null;
+
+    [SerializeField]
+    private Transform m_Canon = null;
+
+    private float m_CurrentTimeBeforeNextFire = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Shoot();
+    }
+
+    public void Shoot()
+    {
+        if(m_CurrentTimeBeforeNextFire <= m_TimeBeforeNextFire)
+        {
+            m_CurrentTimeBeforeNextFire += Time.deltaTime;
+        }
+        else
+        {
+            Instantiate(m_BulletEnemy, m_Canon.position, Quaternion.identity);
+            m_CurrentTimeBeforeNextFire = 0;
+        }
+    }
+}
